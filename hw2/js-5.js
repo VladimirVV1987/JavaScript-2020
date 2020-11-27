87fc9c7973ce8086c458243f9dd22687755b3bd7
vladimir <vladimirvvrg@yandex.ru>
2020-11-27 23:51:54 +0300

    Задание 2

---
+++ hw2/js-5.js
@@ -0,0 +1,20 @@
+"use strict";
+// +
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// -
+function subtractingNumbers(a, b) {
+  return a - b;
+}
+
+// /
+function divideNumbers(a, b) {
+  return a / b;
+}
+
+// *
+function multiplyingNumbers(a, b) {
+  return a * b;
+}
